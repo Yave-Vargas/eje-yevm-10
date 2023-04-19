8860818e8a42d20e57e8ac6ec6658e84986199a0
Yave-Vargas <90014627+Yave-Vargas@users.noreply.github.com>
2023-04-18 21:48:22 -0600

    Primer deploy con Node.js

--- public/js/eje-yevm.js
+++ public/js/eje-yevm.js
@@ -1,21 +1,32 @@
 //Calculadora
-alert('Resultado suma:' + suma);
 function sumaNumeros(){
     //Declaracion let y const 
-    const n1=parseInt(document.getElementById('ent1').value);
-    const n2=parseInt(document.getElementById('ent1').value);
+    const n1=parseFloat(document.getElementById('ent1').value);
+    const n2=parseFloat(document.getElementById('ent2').value);
     //Proceso
-    const suma = n1 + n2;
+    const res = n1 + n2;
     //Salida
-    document.getElementById('resultado').innerHTML= 'Resultado suma:';
-    alert('Resultado suma:' + suma);
+    document.getElementById('resultado').innerHTML= 'Resultado: '+res;
+    //alert('Resultado suma: ' + res);
 }
 function restaNumeros(){
-    
+    const n1=parseFloat(document.getElementById('ent1').value);
+    const n2=parseFloat(document.getElementById('ent2').value);
+    const res = n1 - n2;
+    document.getElementById('resultado').innerHTML= 'Resultado: '+res;
+    //alert('Resultado:' + res);
 }
 function multiNumeros(){
-    
+    const n1=parseFloat(document.getElementById('ent1').value);
+    const n2=parseFloat(document.getElementById('ent2').value);
+    const res = n1 * n2;
+    document.getElementById('resultado').innerHTML= 'Resultado: '+res;
+    //alert('Resultado:' + res);
 }
 function divNumeros(){
-    
+    const n1=parseFloat(document.getElementById('ent1').value);
+    const n2=parseFloat(document.getElementById('ent2').value);
+    const res = n1 / n2;
+    document.getElementById('resultado').innerHTML= 'Resultado: '+res;
+    //alert('Resultado:' + res);
 }
